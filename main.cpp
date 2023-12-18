f5dc01d887066632bc1002b71ba3ba4b0e49f333
James Fehrmann <jfehrmann@outlook.com>
2023-12-18 13:11:07 -0500

    quick commit

---
+++ main.cpp
@@ -0,0 +1,50 @@
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include <string>
+
+void debug(std::string message) { std::cout << message << std::endl; }
+
+std::string printResolutionX() {
+    return std::to_string(sf::VideoMode::getDesktopMode().width);
+}
+
+std::string printResolutionY() {
+    return std::to_string(sf::VideoMode::getDesktopMode().height);
+}
+
+std::string printResolution() {
+    return "Screen Resolution: " + printResolutionX() + "x" + printResolutionY();
+}
+
+std::string printWindowSize(sf::RenderWindow *window) {
+    return "Window Size: " + std::to_string(window->getSize().x) + "x" + std::to_string(window->getSize().y);
+}
+
+std::string printWindowPosition(sf::RenderWindow *window) {
+    return "Window Postion: " + std::to_string(window->getPosition().x) + "x" + std::to_string(window->getPosition().y);
+}
+
+int main() {
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    window.setPosition(sf::Vector2i(2000, 200));
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed)
+                window.close();
+            debug("-----------------------");
+            debug(printResolution());
+            debug(printWindowSize(&window));
+            debug(printWindowPosition(&window));
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
+    return 0;
+}
