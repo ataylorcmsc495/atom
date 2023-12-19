7e4d83284821e1ae71a2f6c9c7747cc17f41b783
James Fehrmann <jfehrmann@outlook.com>
2023-12-19 15:24:13 -0500

    quick commit

--- main.cpp
+++ main.cpp
@@ -33,9 +33,20 @@ std::string printMousePosition(sf::RenderWindow *window) {
 }
 
 int main() {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::Vector2f dim(640.f, 480.f);
+    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML works!");
     window.setPosition(sf::Vector2i(2000, 200));
+    sf::View view(sf::FloatRect(0.f, 0.f, dim.x, dim.y));
+    window.setView(view);
     sf::CircleShape shape(100.f);
+    sf::Texture bgTexture;
+    bgTexture.loadFromFile("vga.bmp");
+    sf::Sprite background;
+    background.setTexture(bgTexture);
+    background.setTextureRect(sf::IntRect(0, 0, (int)dim.x, (int)dim.y));
+
+    background.setScale(1.0f, 1.0f);
+
     shape.setFillColor(sf::Color::Green);
 
     while (window.isOpen()) {
@@ -49,10 +60,24 @@ int main() {
             debug(printWindowPosition(&window));
             debug(printMousePosition());
             debug(printMousePosition(&window));
+            if (event.type == sf::Event::Resized) {
+                view.setSize(dim.x, dim.y);
+                float viewToWindowRatioX = dim.x / event.size.width;
+                float viewToWindowRatioY = dim.y / event.size.height;
+                float letterboxX = (1 - viewToWindowRatioX) / 2;
+                float letterboxY = (1 - viewToWindowRatioY) / 2;
+
+                view.setViewport(sf::FloatRect(letterboxX, letterboxY, viewToWindowRatioX, viewToWindowRatioY));
+                window.setView(view);
+
+                // sf::FloatRect visibleArea(0, 0, event.size.width, event.size.height);
+                // window.setView(sf::View(visibleArea));
+            }
         }
 
         window.clear();
-        window.draw(shape);
+        window.draw(background);
+        //    window.draw(shape);
         window.display();
     }
 
